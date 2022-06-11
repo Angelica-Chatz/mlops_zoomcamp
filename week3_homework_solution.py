af6c50420cafcd9636c5747140d916f8d6a58ca3
deva <deva@ubuntu.0rcvnk0grjbezg5uqopico45sh.ax.internal.cloudapp.net>
2022-06-11 15:16:29 +0000

    model and dictvectorizer pickle objs

--- week3_homework_solution.py
+++ week3_homework_solution.py
@@ -4,22 +4,22 @@ import pandas as pd
 
 import pickle
 
-from prefect import flow, get_run_logger, task
+""" from prefect import flow, get_run_logger, task
 from prefect.task_runners import SequentialTaskRunner
 from prefect.deployments import DeploymentSpec
 from prefect.flow_runners import SubprocessFlowRunner
-from prefect.orion.schemas.schedules import CronSchedule
+from prefect.orion.schemas.schedules import CronSchedule """
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
-def log_task(task_msg):
+""" def log_task(task_msg):
     logger = get_run_logger()
-    logger.info(task_msg)
+    logger.info(task_msg) """
 
-@task
+#@task
 def get_paths(date=None):
 
     path_prefix = "./data/fhv_tripdata_"
@@ -39,17 +39,17 @@ def get_paths(date=None):
     train_path = path_prefix + input_year + '-' + train_month + file_type
 
     #log_task(f"{train_path}, {val_path}")
-    #print(f"{train_path}, {val_path}")
+    print(f"{train_path}, {val_path}")
 
     return train_path, val_path
 
 
-@task
+#@task
 def read_data(path):
     df = pd.read_parquet(path)
     return df
 
-@task
+#@task
 def prepare_features(df, categorical, train=True):
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
@@ -57,16 +57,16 @@ def prepare_features(df, categorical, train=True):
 
     mean_duration = df.duration.mean()
     if train:
-        log_task(f"The mean duration of training is {mean_duration}")
-        #print(f"The mean duration of training is {mean_duration}")
+        #log_task(f"The mean duration of training is {mean_duration}")
+        print(f"The mean duration of training is {mean_duration}")
     else:
-        log_task(f"The mean duration of validation is {mean_duration}")
-        #print(f"The mean duration of validation is {mean_duration}")
+        #log_task(f"The mean duration of validation is {mean_duration}")
+        print(f"The mean duration of validation is {mean_duration}")
     
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
-@task
+#@task
 def train_model(df, categorical):
 
     train_dicts = df[categorical].to_dict(orient='records')
@@ -74,21 +74,21 @@ def train_model(df, categorical):
     X_train = dv.fit_transform(train_dicts) 
     y_train = df.duration.values
     
-    log_task(f"The shape of X_train is {X_train.shape}")
-    #print(f"The shape of X_train is {X_train.shape}")
+    #log_task(f"The shape of X_train is {X_train.shape}")
+    print(f"The shape of X_train is {X_train.shape}")
     
-    log_task(f"The DictVectorizer has {len(dv.feature_names_)} features")
-    #print(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    #log_task(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    print(f"The DictVectorizer has {len(dv.feature_names_)} features")
 
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_train)
     mse = mean_squared_error(y_train, y_pred, squared=False)
-    log_task(f"The MSE of training is: {mse}")
-    #print(f"The MSE of training is: {mse}")
+    #log_task(f"The MSE of training is: {mse}")
+    print(f"The MSE of training is: {mse}")
     return lr, dv
 
-@task
+#@task
 def run_model(df, categorical, dv, lr):
     val_dicts = df[categorical].to_dict(orient='records')
     X_val = dv.transform(val_dicts) 
@@ -96,14 +96,14 @@ def run_model(df, categorical, dv, lr):
     y_val = df.duration.values
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    log_task(f"The MSE of validation is: {mse}")
-    #print(f"The MSE of validation is: {mse}")
+    #log_task(f"The MSE of validation is: {mse}")
+    print(f"The MSE of validation is: {mse}")
     return
 
-@flow(task_runner=SequentialTaskRunner())
+#@flow(task_runner=SequentialTaskRunner())
 def main(date=None):
 
-    train_path, val_path = get_paths(date).result()
+    train_path, val_path = get_paths(date)
 
     categorical = ['PUlocationID', 'DOlocationID']
 
@@ -114,7 +114,7 @@ def main(date=None):
     df_val_processed = prepare_features(df_val, categorical, False)
 
     # train the model
-    lr, dv = train_model(df_train_processed, categorical).result()
+    lr, dv = train_model(df_train_processed, categorical)#.result()
     run_model(df_val_processed, categorical, dv, lr)
 
     with open(f"models/dv-{date}.b", "wb") as f_out:
@@ -127,11 +127,11 @@ def main(date=None):
 main(date="2021-03-15")
 
 
-# preferct deployment
-DeploymentSpec(
+# prefect deployment
+""" DeploymentSpec(
     name="model_training_prefect",
     flow_location="/home/deva/mlops_zoomcamp/week3_homework_solution.py",
     schedule=CronSchedule(cron="0 9 15 * *"),
     flow_runner=SubprocessFlowRunner(),
     tags=["prefect_hw3"]
-)
+) """
